071c69d4d2ff4f42cc706389d88f991d89d3c193
gelly <gelseyland@gmail.com>
2026-06-16 20:04:07 -0400

    Blog is finally fixedgit add . And I also finally learned how to use auto change log!

--- src/content.config.ts
+++ src/content.config.ts
@@ -2,7 +2,7 @@ import { defineCollection, z } from "astro:content";
 import { glob } from "astro/loaders";
 
 const blog = defineCollection({
-    type: "content",
+    loader: glob({ pattern: "**/*.md", base: "./src/content/blog"}),
     schema: z.object({
         title: z.string(),
         pubDate: z.date(), 
